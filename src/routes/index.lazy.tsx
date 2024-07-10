bbbc4df459a73f3bee8043ceff718d0972f7dd41
Vanessa Tejerina <vanetejerina314@gmail.com>
2024-07-09 21:55:28 -0400

    Home page - Added space between sections on mobile

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -13,7 +13,7 @@ function Index() {
             <section className='flex flex-col items-center gap-4'>
                 <h2 className='text-xl font-semibold'>Nuestros Aliados</h2>
                 <NuestrosAliados />
-                <section className='flex flex-col w-full'>
+                <section className='flex flex-col w-full gap-4 md:gap-0'>
                     {infoSections.map((section) => (
                         <InfoSection key={section.title} {...section} />
                     ))}
