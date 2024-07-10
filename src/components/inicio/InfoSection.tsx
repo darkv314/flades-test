f6791071e51b66188c4483d63ea864f20dc8b290
Vanessa Tejerina <60781992+darkv314@users.noreply.github.com>
2024-07-10 11:01:21 -0400

    Fixed small sections in Home UI

--- src/components/inicio/InfoSection.tsx
+++ src/components/inicio/InfoSection.tsx
@@ -17,12 +17,12 @@ export type InfoSectionProps = {
 
 function InfoSection({ index, slider = false, imgSrc, imgTitle, gallery = [], title, description, button = true, customContent }: InfoSectionProps) {
     return (
-        <section className="h-[50svh] md:min-h-[100svh] w-full relative">
+        <section className="min-h-[50svh] md:min-h-[100svh] w-full relative">
             {
                 slider ? <FadeImageSlider slides={gallery} /> :
                     <img className="size-full absolute object-cover" src={imgSrc} alt={imgTitle} />
             }
-            <div className={clsx("xl:w-[20%] lg:w-[30%] md:w-[40%] w-full z-10 p-4 flex flex-col justify-center absolute text-white min-h-full md:min-h-[100svh] bg-[#0000003d] backdrop-blur-[1px] md:backdrop-blur-md shadow-section gap-10",
+            <div className={clsx("xl:w-[20%] lg:w-[30%] md:w-[40%] w-full z-10 p-4 flex flex-col justify-center relative md:absolute text-white min-h-full md:min-h-[100svh] bg-[#0000003d] backdrop-blur-[1px] md:backdrop-blur-md shadow-section gap-10",
                 index % 2 !== 0 ? "md:right-[50px]" : "md:left-[50px]")}>
                 <h2 className="text-3xl md:text-5xl font-semibold text-balance break-words">
                     {title}
