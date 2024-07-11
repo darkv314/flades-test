f1c45b52fc193b8cb93ea6478eaa6410bb172f01
Vanessa Tejerina <vanetejerina314@gmail.com>
2024-07-11 02:01:27 -0400

    Added scroll restoration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Outlet, ScrollRestoration } from '@tanstack/react-router'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Navbar from '../components/shared/Navbar'
 
 export const Route = createRootRoute({
     component: () => (
         <main className='min-h-[100dvh] font-poppins'>
             <Navbar />
+            <ScrollRestoration />
             <Outlet />
-            <TanStackRouterDevtools />
+            {/* <TanStackRouterDevtools /> */}
         </main>
     ),
 })
